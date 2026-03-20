e88be9bec4967c0c2adca5ec27bc4aa18b618103
Татьяна <ztatyana1995@yandex.ru>
2026-03-20 22:26:41 +0300

    настроила allure

--- tests/test_pets_positive.py
+++ tests/test_pets_positive.py
@@ -1,22 +1,32 @@
-import pytest
+import allure
 from resources.data_generator import generate_pet_payload, generate_data_for_update, generate_pet_update_payload
 from resources.pet_api import ApiPet
 
 def test_create_new_pet():
     payload = generate_pet_payload()
     pets = ApiPet()
-    create_response = pets.add_a_new_pet(payload)
-    assert create_response.status_code == 200, "Статус код должен быть 200"
+
+    with allure.step("Создать нового питомца"):
+        create_response = pets.add_a_new_pet(payload)
+
+    with allure.step("Запрос создания отдает 200"):
+        assert create_response.status_code == 200, "Статус код должен быть 200"
+
     create_body = create_response.json()
     pet_id = create_body["id"]
     print(pet_id)
-    assert pet_id == payload["id"]
 
-    get_response = pets.get_pet_by_id(pet_id)
-    assert get_response.status_code == 200, "Статус код должен быть 200"
+    with allure.step("Запросить созданного питомца по id"):
+        get_response = pets.get_pet_by_id(pet_id)
+    with allure.step("Запрос получения по id отдает 200"):
+        assert get_response.status_code == 200, "Статус код должен быть 200"
+
     body = get_response.json()
-    assert body["id"] == pet_id, "id не соответсвует запрошенному"
-    assert body["category"]["id"] == payload["category"]["id"], "id категории не соответсвует"
+
+    with allure.step("Проверка, что id полученного питомца совпадает с id созданного"):
+        assert body["id"] == pet_id, "id не соответсвует запрошенному"
+    with allure.step("Проверка, что id категории полученного питомца совпадает с id категорией созданного"):
+        assert body["category"]["id"] == payload["category"]["id"], "id категории не соответсвует"
     assert body["category"]["name"] == payload["category"]["name"], "имя категории не соответсвует"
     assert body["name"] == payload["name"], "имя не соответсвует"
     assert body["photoUrls"] == payload["photoUrls"], "ссылка на изображение не соответсвует"
@@ -24,7 +34,6 @@ def test_create_new_pet():
     assert body["tags"][0]["name"] == payload["tags"][0]["name"], "название тега не соответсвует"
     assert body["status"] == payload["status"], "статус не соответсвует"
 
-@pytest.mark.delete
 def test_delete_pet(created_pet):
     pet_id = created_pet["pet_id"]
     print(pet_id)
@@ -40,7 +49,6 @@ def test_delete_pet(created_pet):
     assert get_body["type"] == "error"
     assert get_body["message"] == "Pet not found"
 
-@pytest.mark.get
 def test_get_pet_by_status(created_pet):
     payload = created_pet["payload"]
     pet_status = payload["status"]
@@ -79,7 +87,6 @@ def test_update_pet_with_form_data(created_and_delete_pet):
     assert body["tags"][0]["name"] == payload["tags"][0]["name"], "название тега не соответсвует"
     assert body["status"] == data["status"], "статус не соответсвует"
 
-@pytest.mark.upd
 def test_update_pet(created_pet):
     pet_id=created_pet["pet_id"]
     print(pet_id)
